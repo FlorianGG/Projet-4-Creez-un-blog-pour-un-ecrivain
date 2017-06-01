aa15ec4dcd53b3f093ca0bb2ce25cff12c797c73
Florian Garcia <florian.prof@gmail.com>
2017-06-01 16:01:01 +0200

    Correction erreur index.php
    Correction sur la condition du if qui affiche indexController.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 
 
 
-    if ($_GET['action'] === 'index' || $_GET['action'] !== 'index' || empty($_GET['action'])) {
+    if ($_GET['action'] === 'index' || ($_GET['action'] !== 'index' & $_GET['action'] !== 'show') || empty($_GET['action'])) {
     	include('controller/indexController.php');
     }elseif ($_GET['action'] === 'show') {
     	include('controller/article.php');
