6238b97fe9ade0a8c772586b62eeea6c5ffaaf1d
Florian Garcia <florian.prof@gmail.com>
2017-06-01 14:41:21 +0200

    Changer le nom du fichier listArticles.php
    Pour coller aux instructions, j'ai changer le nom du fichier listArticles.php en indexController.php. J'ai également changé le nom dans l'include de index

--- index.php
+++ index.php
@@ -6,11 +6,9 @@
 
 
     if ($_GET['action'] === 'index' || $_GET['action'] !== 'index' || empty($_GET['action'])) {
-    	include('controller/listArticles.php');
+    	include('controller/indexController.php');
     }elseif ($_GET['action'] === 'show') {
     	include('controller/article.php');
     }
-
-
 
 ?>
