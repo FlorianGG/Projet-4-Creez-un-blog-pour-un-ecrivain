193568b5546edebe50304a44b02e1b127c422c5a
Nicolas MUGNIER <mugnier.nicolas@gmail.com>
2017-06-01 20:30:22 +0200

    update index.php

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
-    include('vendor/autoload.php');
 
-    include('controller/header.php');
+    require_once('vendor/autoload.php');
 
-
-
-    if ($_GET['action'] === 'index' || ($_GET['action'] !== 'index' && $_GET['action'] !== 'show') || empty($_GET['action'])) {
-    	include('controller/indexController.php');
-    }elseif ($_GET['action'] === 'show') {
-    	include('controller/article.php');
+    // http://localhost/controller={controller}&action={action}
+    switch($_GET['controller']){
+        case 'article':
+            $controller = new ArticleController();
+            $action = $_GET['action'].'Action';
+            echo $controller->$action();
+            break;
+        default:
+            break;
     }
-
-?>
