b7308eeafa3c7f4ad8bf100dbe8405a3107bdfb6
Florian Garcia <florian.prof@gmail.com>
2017-06-23 12:32:00 +0200

    Refactore the methods of the controller
    Using of only one return my function

--- controller/ArticleController.php
+++ controller/ArticleController.php
@@ -40,7 +40,6 @@
 			$id = (int)$this->request->getParam('id');
 			if (is_null($id) OR !isset($id)) {
 				$html = "<h2>Article introuvalbe </h2>";
-				return $this->response->setBody($html);
 			}else{
 				// $article = (new Article)->load($id); à mettre en place
 				$ArticleManager = new ArticleManager;
@@ -48,18 +47,17 @@
 				//si aucune erreur on affiche l'article selectionné
 				if (!is_null($article)){
 					$html = "";
-
 					$html .= '<h2>' . $article->getTitle() . '</h2>';
 					$html .= '<p>' . $article->getContent() . '</p>';
 					$html .= '<p><em> Crée le : ' . $article->getDateArticle() . '</em></p>';
 
-					return $this->response->setBody($html);
 				//dans tous les cas d'erreur on affiche que l'article est introuvable
 				}else{
 					$html = "<h2>Article introuvalbe </h2>";
-					return $this->response->setBody($html);
 				}
 			}
+			//on return le $html
+			return $this->response->setBody($html);
 		}
 
 		// http://localhost?controller=article&action=delete&id=3
@@ -70,22 +68,20 @@
 			$id = (int) $this->request->getParam('id');
 			if (is_null($id) OR !isset($id) OR $id === 0) {
 				$html = "<h2>Article introuvalbe </h2>";
-				return $this->response->setBody($html);
 			}else{
 				$ArticleManager = new ArticleManager;
 				$article = $ArticleManager->delete($id);
 				if ($article) {
 					$html = "<h2>L'article n°" . $id . " a bien été supprimé</h2";
-					return $this->response->setBody($html);
 				}elseif (is_null($article)) {
 					$html = "<h2>Article introuvable</h2>";
-					return $this->response->setBody($html);
 				}
 				else{
 					$html = "Il y a eu une erreur d'éxécution, veuillez vérifier vos paramètres.";
-					return $this->response->setBody($html);
 				}
 			}
+			//on return le $html
+			return $this->response->setBody($html);
 		}
 
 		// http://localhost?controller=article&action=addArticle
@@ -104,7 +100,7 @@
 				$html = 'Une erreur est survenue';
 			}
 
-			
+			//on return le $html			
 			return $this->response->setBody($html);
 
 		}
