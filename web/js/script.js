dad28902e8bb50061c550849fa6370594b3d0b01
Florian Garcia <florian.prof@gmail.com>
2017-07-19 12:40:06 +0200

    Add new fonctionnality in jQuery in order the display or not the children comments

--- web/js/script.js
+++ web/js/script.js
@@ -4,6 +4,16 @@ $(document).ready(function(){
         $(this).next('.formComment').toggle();
     });
 });
+$(document).ready(function(){
+	$(".buttonDisplay").click(function(){
+		$(this).next('.children').slideToggle();
+		if ($(this).text() === 'Afficher réponses') {
+			$(this).text('Cacher réponses');
+		}else{
+			$(this).text('Afficher réponses');
+		}
+	});
+});
 
 
 
